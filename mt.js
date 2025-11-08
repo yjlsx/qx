9212084ebc504179dab3ef0d55d834ba25216877
yjlsx <2413704955@qq.com>
2025-11-09 00:08:55 +0800

    .

--- mt.js
+++ mt.js
@@ -19,9 +19,9 @@ hostname = i.waimai.meituan.com, *.meituan.com
 */
 
 // === 🧭 你只要改这里 ===
-const CUSTOM_ORDER_TIME = "2025-11-08 09:12:25"; // 🕐 下单时间（精确到秒）
-const TARGET_ORDER_ID_NUM = 601891042418251030;   // 新订单号
-const TARGET_ARRIVAL_TIME = "11月8日 10:00-10:20"; // 期望送达时间
+const CUSTOM_ORDER_TIME = "2025-11-09 09:12:25"; // 🕐 下单时间（精确到秒）
+const TARGET_ORDER_ID_NUM = 604248182418564450;   // 新订单号
+const TARGET_ARRIVAL_TIME = "11月9日 10:00-10:20"; // 期望送达时间
 // =====================
 
 // 自动生成字符串ID
