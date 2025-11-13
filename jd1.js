98fc417f7a09656ef7c0cf5f2a21c9260df25cc0
yjlsx <2413704955@qq.com>
2025-11-13 10:13:09 +0800

    Update jd1.js

--- jd1.js
+++ jd1.js
@@ -26,7 +26,7 @@ const DETAIL_NEW_ORDER_ID = "326268478613"; // 详情页显示的新订单编号
 const DETAIL_NEW_DATE_BASE = "2025-11-13";  // 新的日期
 const DETAIL_NEW_TIME_BASE = "09:19:20";   // 新的基本时间 (用于下单时间)
 const DETAIL_NEW_PAY_TIME = "2025-11-13 09:20:55"; // 新的支付时间
-const DETAIL_NEW_COMPLETE_TIME = "2025-11-13 10:03:04"; // 新的订单完成/送达时间
+const DETAIL_NEW_COMPLETE_TIME = "2025-11-13 10:13:04"; // 新的订单完成/送达时间
 const DETAIL_NEW_EXPECTED_DELIVERY_TIME = "2025-11-13 10:00-10:20"; // 新的期望配送时间
 const DETAIL_NEW_CURRENT_TIME = "2025-11-13 09:21:22"; // 模拟服务器返回的当前时间
 
