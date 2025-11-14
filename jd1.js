df18b6b28b0a825701c6ade144bce007a3d993ed
yjlsx <2413704955@qq.com>
2025-11-14 11:18:15 +0800

    Update jd1.js

--- jd1.js
+++ jd1.js
@@ -27,7 +27,7 @@ const DETAIL_NEW_DATE_BASE = "2025-11-14";  // 新的日期
 const DETAIL_NEW_TIME_BASE = "10:45:20";   // 新的基本时间 (用于下单时间)
 const DETAIL_NEW_PAY_TIME = "2025-11-14 10:46:55"; // 新的支付时间
 const DETAIL_NEW_COMPLETE_TIME = "2025-11-14 11:15:04"; // 新的订单完成/送达时间
-const DETAIL_NEW_EXPECTED_DELIVERY_TIME = "2025-11-14 11:10-10:25"; // 新的期望配送时间
+const DETAIL_NEW_EXPECTED_DELIVERY_TIME = "2025-11-14 11:10-11:25"; // 新的期望配送时间
 const DETAIL_NEW_CURRENT_TIME = "2025-11-14 10:47:22"; // 模拟服务器返回的当前时间
 
 // ===================================
