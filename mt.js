1e513087847c7ddc7d27a581a635d616da467dc8
yjlsx <2413704955@qq.com>
2025-11-19 16:46:05 +0800

    Update mt.js

--- mt.js
+++ mt.js
@@ -21,9 +21,9 @@ hostname = i.waimai.meituan.com, *.meituan.com, wx-shangou.meituan.com
 
 
 // === 🧭 你只要改这里 ===
-const CUSTOM_ORDER_TIME = "2025-11-19 16:37:41";    // 🕐 下单时间（精确到秒）
-const TARGET_ORDER_ID_NUM = "601869801554478301";   // 新订单号（用于详情页显示）
-const TARGET_ARRIVAL_TIME = "11月19日 17:15-17:30"; // 期望送达时间
+const CUSTOM_ORDER_TIME = "2025-11-19 16:41:15";    // 🕐 下单时间（精确到秒）
+const TARGET_ORDER_ID_NUM = "601869802357841766";   // 新订单号（用于详情页显示）
+const TARGET_ARRIVAL_TIME = "11月19日 17:22-17:37"; // 期望送达时间
 // =====================
 
 // 自动生成字符串ID
