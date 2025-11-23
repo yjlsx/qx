2119bf9390006a8ebbf4d1298b3449ac3cb7939d
yjlsx <2413704955@qq.com>
2025-11-23 12:21:15 +0800

    Update jd1.js

--- jd1.js
+++ jd1.js
@@ -21,14 +21,14 @@ hostname = api.m.jd.com
 // ⚙️ 用户可配置项 (订单详情页)
 // ===================================
 
-const DETAIL_NEW_ORDER_ID = "326769837924"; // 详情页显示的新订单号
+const DETAIL_NEW_ORDER_ID = "326925369765"; // 详情页显示的新订单号
 
 const DETAIL_NEW_DATE_BASE = "2025-11-23";  // 新的日期
-const DETAIL_NEW_TIME_BASE = "00:14:33";   // 新的基本时间 (用于下单时间)
-const DETAIL_NEW_PAY_TIME = "2025-11-23 00:15:42"; // 新的支付时间
-const DETAIL_NEW_COMPLETE_TIME = "2025-11-23 00:55:41"; // 新的订单完成/送达时间
-const DETAIL_NEW_EXPECTED_DELIVERY_TIME = "2025-11-23 00:50-01:05"; // 新的期望配送时间
-const DETAIL_NEW_CURRENT_TIME = "2025-11-23 00:16:12"; // 模拟服务器返回的当前时间
+const DETAIL_NEW_TIME_BASE = "12:17:33";   // 新的基本时间 (用于下单时间)
+const DETAIL_NEW_PAY_TIME = "2025-11-23 12:18:33"; // 新的支付时间
+const DETAIL_NEW_COMPLETE_TIME = "2025-11-23 12:53:14"; // 新的订单完成/送达时间
+const DETAIL_NEW_EXPECTED_DELIVERY_TIME = "2025-11-23 12:50-13:00"; // 新的期望配送时间
+const DETAIL_NEW_CURRENT_TIME = "2025-11-23 12:19:32"; // 模拟服务器返回的当前时间
 
 // ===================================
 // 🛠️ 脚本主体
