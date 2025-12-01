043fc548182787e93ee07e29a516e86a437d3021
yjlsx <2413704955@qq.com>
2025-12-01 12:17:15 +0800

    Update wm.js

--- write/wm.js
+++ write/wm.js
@@ -5,7 +5,8 @@
 ####################################################################
 
 [rewrite_local]
-^https:\/\/wmapp-api\.waimaimingtang\.com\/fission\/account\/balance\/ url script-response-body https://raw.githubusercontent.com/yjlsx/qx/refs/heads/main/write/wm.js
+^https:\/\/wmapp-api\.waimaimingtang\.com\/fission\/account\/balance\/ url script-response-body wm_modify.js
+^https:\/\/wmapp-api\.waimaimingtang\.com\/api\/api\/v2\/user\/cancelAccount$ url script-response-body wm_modify.js
 
 [mitm]
 hostname = wmapp-api.waimaimingtang.com
@@ -14,16 +15,29 @@ hostname = wmapp-api.waimaimingtang.com
 */
 
 /*
- * 修改外卖明堂账户余额为 0
+ * 外卖明堂余额 + 注销修改脚本
  */
 
+let url = $request.url;
 let body = $response.body;
 
 try {
     let obj = JSON.parse(body);
 
-    if (obj && obj.data && typeof obj.data.accountBalance !== "undefined") {
-        obj.data.accountBalance = 0; // 修改余额
+    // 修改余额接口
+    if (/\/fission\/account\/balance\//.test(url)) {
+        if (obj && obj.data) {
+            obj.data.accountBalance = 0;
+            obj.data.totalBalance = 0;
+        }
+    }
+
+    // 修改注销接口
+    if (/\/user\/cancelAccount$/.test(url)) {
+        obj.code = 1;
+        obj.success = true;
+        obj.message = "注销成功";
+        obj.data = null;
     }
 
     $done({ body: JSON.stringify(obj) });
@@ -32,4 +46,3 @@ try {
     console.log("解析响应失败：" + e);
     $done({ body });
 }
-
