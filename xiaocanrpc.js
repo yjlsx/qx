a280a62726e0649456e3788c4950d51dd1c42e93
yjlsx <2413704955@qq.com>
2026-01-31 12:10:44 +0800

    Update xiaocanrpc.js

--- xiaocanrpc.js
+++ xiaocanrpc.js
@@ -10,45 +10,36 @@ hostname = gw.xiaocantech.com
 */
 
 
-
 /**
- * 小蚕助手 - 登录转游客重写 (强制修正版)
- * 配置: script-request-header
+ * 小蚕助手 - 登录转游客重写 (静默修正版)
+ * 逻辑：只改参数，不弹通知，防止死循环干扰
  */
 
 let headers = $request.headers;
 let bodyObj = JSON.parse($request.body || "{}");
 let mName = headers['methodname'] || headers['Methodname'] || "";
 
-// 定义要伪装的查询和搜索接口
-const targetMethods = [
-    "RecService.GetStorePromotionList", 
-    "RecService.SearchStorePromotionList",
-    "SilkwormRcsService.MeituanShangjinGetPoiList"
-];
+// 精准锁定首页列表和搜索这两个关键接口
+const isTarget = mName.indexOf("GetPoiList") > -1 || mName.indexOf("PromotionList") > -1;
 
-if (targetMethods.some(m => mName.indexOf(m) > -1)) {
-    console.log(" 正在转换游客查询身份: " + mName);
+if (isTarget) {
+    console.log(" 正在静默重写身份: " + mName);
 
-    // 1. 核心操作：抹除所有与你账号相关的标识
-    // 很多时候，服务器发现 X-Sivir 为空，会回退到基础校验
+    // 1. 尝试“脱壳”：只抹除身份标识，保留签名所需的其他环境
     delete headers['X-Sivir'];
     delete headers['x-sivir'];
     headers['X-Vayne'] = '0';
     headers['x-Teemo'] = '0';
     
-    // 2. 修正 Body
+    // 2. 修正 Body 里的 ID
     if (bodyObj.hasOwnProperty("silk_id")) bodyObj["silk_id"] = 0;
     if (bodyObj.hasOwnProperty("user_id")) bodyObj["user_id"] = 0;
-
-    // 3. 弹窗确认重写已触发
-    $notify("小蚕重写", "身份已切换为游客", "正在请求全量活动列表...");
 
     $done({
         headers: headers,
         body: JSON.stringify(bodyObj)
     });
 } else {
-    // 抢名额接口严禁重写，否则领不到号上
+    // 其他接口（如抢名额、心跳、埋点）一律原样跳过，不弹通知
     $done({});
 }
